51791a406bb8b172f337accce44f42e1d572f3af
John Olawale <mojoblanco@gmail.com>
2017-07-09 00:14:07 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="intro text-center">
+                <p>Want to become the first <span class="outline">DECODED</span> act?</p>
+                <p>Simply download the <span class="outline">ALAT</span> and sign up.</p>
+                <a href="#" class="btn btn-primary btn-lg">Submit Entry</a>
+            </div>
+        </div>
+    </div>
+@endsection
